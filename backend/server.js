2d4264b06cb0f960487ecd979601d00f370dbbe8
Nicholas McCooey <nicholasmccooey@gmail.com>
2020-10-15 14:23:48 -0400

    Connected to MongoDB.

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 import products from "./data/products.js";
+import colors from "colors";
 
 dotenv.config();
+
+connectDB();
+
 const app = express();
 
 app.get("/", (req, res) => {
@@ -21,5 +26,7 @@ app.get("/api/products/:id", (req, res) => {
 const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
